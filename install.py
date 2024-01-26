f9353068fc242eabfda26bfdb617aefa2c74e8b3
Ghost King <157604770+GhostKing47@users.noreply.github.com>
2024-01-26 12:18:37 -0300

    Update install.py

--- install.py
+++ install.py
@@ -11,3 +11,5 @@ os.system('pip install nltk')
 # Instalando "neofetch"
 print("\033[91m#Instalando neofetch\033[0m")
 os.system('pkg install neofetch')
+
+print("\033[91mPara executar o Script usse o comando: python3 Chat.py\033[0m")
